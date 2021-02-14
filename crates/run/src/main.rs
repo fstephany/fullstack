eedc0c7375b0fcf375ebc0452bc54848c11abfcb
Francois Stephany <francois@tamere.eu>
2021-02-14 18:55:56 +0100

    Add some more notes about general feeling

--- crates/run/src/main.rs
+++ crates/run/src/main.rs
@@ -5,10 +5,20 @@ use wasm_run::DefaultServeArgs;
 
 #[wasm_run::main(
     "frontend",
-    run_server = run_server
+    run_server = run_server,
+    post_build = post_build
 )]
 #[derive(StructOpt, Debug)]
 enum Cli {}
+
+fn post_build(
+    args: &DefaultBuildArgs,
+    _profile: BuildProfile,
+    wasm_js: String,
+    wasm_bin: Vec<u8>,
+) -> anyhow::Result<()> {
+    Ok(())
+}
 
 fn run_server(_args: DefaultServeArgs) -> anyhow::Result<()> {
     println!("Woaw");
